6eb5c48279a6f912d8f857629afc229c4d4e295e
Ivan <63307610+ShulaevIvan@users.noreply.github.com>
2022-04-26 21:57:57 +0300

    Update urls.py

--- measurement/urls.py
+++ measurement/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import MeasurementView, SensorView, MeasurementDetailView, UpdateSensorView
+from .views import MeasurementView, SensorView, MeasurementDetailView
 urlpatterns = [
     path('sensors/', SensorView.as_view()),
     path('sensors/<pk>/', MeasurementDetailView.as_view()),
-    path('sensors/<pk>/', UpdateSensorView.as_view()),
     path('measurements/', MeasurementView.as_view()),
 ]
