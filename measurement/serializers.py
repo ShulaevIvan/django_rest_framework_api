f94a2961478fd3406537e59389aaf1338d77d6e7
Ivan <63307610+ShulaevIvan@users.noreply.github.com>
2022-04-26 21:56:10 +0300

    Update serializers.py

--- measurement/serializers.py
+++ measurement/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import Sensor, Measurement
-# TODO: опишите необходимые сериализаторы
 
 class SensorSerializer(serializers.ModelSerializer):
     
